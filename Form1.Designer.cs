ab081aa1887c42205af09d240805fa0710facf3a
sai wanza <q020385791@gmail.com>
2019-11-19 11:25:19 +0800

    加入專案檔案。

---
+++ Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Nmaereport2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbYear = new System.Windows.Forms.ComboBox();
+            this.cbMonth = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 381);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 57);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "創建簽到表";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 222);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "年份";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(218, 222);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "月份";
+            // 
+            // cbYear
+            // 
+            this.cbYear.FormattingEnabled = true;
+            this.cbYear.Location = new System.Drawing.Point(68, 219);
+            this.cbYear.Name = "cbYear";
+            this.cbYear.Size = new System.Drawing.Size(121, 26);
+            this.cbYear.TabIndex = 3;
+            // 
+            // cbMonth
+            // 
+            this.cbMonth.FormattingEnabled = true;
+            this.cbMonth.Location = new System.Drawing.Point(274, 219);
+            this.cbMonth.Name = "cbMonth";
+            this.cbMonth.Size = new System.Drawing.Size(121, 26);
+            this.cbMonth.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cbMonth);
+            this.Controls.Add(this.cbYear);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbYear;
+        private System.Windows.Forms.ComboBox cbMonth;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,208 @@
+﻿using Microsoft.Office.Interop.Word;
+using System;
+using System.IO;
+using System.Windows.Forms;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace Nmaereport2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Object oMissing = System.Reflection.Missing.Value;
+            Object oEndOfDoc = "\\endofdoc";
+            Microsoft.Office.Interop.Word.Application wrdApp;
+
+            Microsoft.Office.Interop.Word._Document myDoc;
+            wrdApp = new Microsoft.Office.Interop.Word.Application();
+            //執行過程不在畫面上開啟 Word
+            wrdApp.Visible = false;    
+            myDoc = wrdApp.Documents.Add(ref oMissing, ref oMissing, ref oMissing, ref oMissing);
+            //邊界設定
+            //上邊界
+            wrdApp.ActiveDocument.PageSetup.TopMargin = 23F;
+
+            //左邊界
+            wrdApp.ActiveDocument.PageSetup.LeftMargin = 20F;
+
+            //下邊界
+            wrdApp.ActiveDocument.PageSetup.BottomMargin = 10F;
+
+            //右邊界
+            wrdApp.ActiveDocument.PageSetup.RightMargin = 20F;
+
+            //加入文字
+            Microsoft.Office.Interop.Word.Paragraph oPara;
+            object oRng = myDoc.Bookmarks.get_Item(ref oEndOfDoc).Range;
+            oPara = myDoc.Content.Paragraphs.Add(ref oRng);
+            oPara.Range.Text = "附件6";
+
+            //設定粗體
+            oPara.Range.Font.Bold = 1;
+            //oPara.Format.SpaceAfter = 6;
+
+            //靠右對齊
+            oPara.Alignment = Microsoft.Office.Interop.Word.WdParagraphAlignment.wdAlignParagraphRight;
+            oPara.Range.InsertParagraphAfter();
+
+            wrdApp.Application.Selection.MoveEnd();
+            object count = 14;
+            //换一行;
+            object WdLine = Word.WdUnits.wdLine;
+            wrdApp.Selection.MoveDown(ref WdLine, ref count, ref oMissing);//移动焦点
+            //wrdApp.Selection.TypeParagraph();//插入段落
+            oPara.Range.Font.Size = 16;
+            oPara.Range.Font.Bold = 0;
+
+            //Selected Year and Month
+            int Year = (int)(Convert.ToInt64(cbYear.Text));
+            int month = (int)Convert.ToInt64(cbMonth.Text);
+
+
+            //靠左對齊
+            oPara.Alignment = Microsoft.Office.Interop.Word.WdParagraphAlignment.wdAlignParagraphLeft;
+            oPara.Range.Text = (Year - 1911).ToString() + "年" + month + "月份              廠商駐點簽到表";
+            oPara.Range.InsertParagraphAfter();
+
+            //移动焦点
+            wrdApp.Selection.MoveDown(ref WdLine, ref count, ref oMissing);
+
+            //字體大小
+            oPara.Range.Font.Size = 12;
+
+            //字體樣式
+            oPara.Range.Font.Name = "標楷體";
+
+            //創建Table
+            //Create Table
+            //Create a Table with one row 、 six Column
+            Table table = oPara.Range.Tables.Add(wrdApp.Selection.Range, 1, 6, oMissing, ref oMissing);
+
+            //每一行的高度
+            table.Rows.Height = 25f;
+
+            //內框線樣式
+            table.Borders.InsideLineStyle = Word.WdLineStyle.wdLineStyleSingle;
+
+            //外框線樣式
+            table.Borders.OutsideLineStyle = Word.WdLineStyle.wdLineStyleSingle;
+
+            //外框線粗度
+            table.Borders.OutsideLineWidth = WdLineWidth.wdLineWidth150pt;
+
+            //First Line(Title)
+            //表格第一行(標題)
+            table.Cell(1, 1).Range.Text = "日期";
+            //All Line Fill Gray Color
+            //整行加入灰色
+            table.Cell(1, 1).Row.Shading.BackgroundPatternColor = WdColor.wdColorGray125;
+            table.Cell(1, 2).Range.Text = "廠商人員簽到";
+            table.Cell(1, 3).Range.Text = "到院時間";
+            table.Cell(1, 4).Range.Text = "離院時間";
+            table.Cell(1, 5).Range.Text = "資訊部門\n代表簽章";
+            table.Cell(1, 6).Range.Text = "備註";
+
+            //紀錄已經記錄到陣列的第幾位
+            int CountLocationday = 0;
+            //紀錄星期五在第幾行 塞到陣列去
+            int[] LocationFriday = new int[] { 0, 0, 0, 0, 0 };
+            for (int i = 0; i < DateTime.DaysInMonth(Year, month); i++)
+            {
+                //Get the DataTime You Selected
+                //取得所選的時間基準年月日
+                DateTime dt = new DateTime(Year, month, i + 1);
+
+
+                //Only Add row between Monday and Friday 
+                //如果不是星期六或是星期日才增加一行欄位
+                if (dt.DayOfWeek != DayOfWeek.Saturday && dt.DayOfWeek != DayOfWeek.Sunday)
+                {
+                    var row = table.Rows.Add();
+
+                    //背景設定回白色，因為第一行原本背景為白色
+                    row.Shading.BackgroundPatternColor = WdColor.wdColorWhite;
+                    row.Range.Text = month + "/" + (i + 1).ToString();
+                    if (dt.DayOfWeek == DayOfWeek.Friday)
+                    {
+                        int CountFriday = row.Index;
+                        LocationFriday.SetValue(CountFriday, CountLocationday);
+                        CountLocationday++;
+
+                        //It's not working,but I can't find reason,so I use other way
+                        //這要畫框線沒用只有對最後一個有用 不知道為啥
+                        //var TestIndex = row.Index;
+                        //table.Cell(TestIndex, i).Range.Borders[WdBorderType.wdBorderBottom].LineWidth = WdLineWidth.wdLineWidth150pt;
+                    }
+                }
+            }
+
+            //上面挑選完星期五在哪邊，就可以開始畫粗黑框了
+            //注意 寫在上面if (dt.DayOfWeek == DayOfWeek.Friday)那邊是沒有用的，
+            foreach (var Friday in LocationFriday)
+            {
+                if (Friday != 0)
+                {
+                    for (int i = 1; i <= 6; i++)
+                    {
+                        table.Cell(Friday, i).Range.Borders[WdBorderType.wdBorderBottom].LineWidth = WdLineWidth.wdLineWidth150pt;
+                    }
+                }
+            }
+                string dummyFileName = "SaveHere";
+
+                SaveFileDialog sf = new SaveFileDialog();
+                sf.FileName = dummyFileName;
+                if (sf.ShowDialog() == DialogResult.OK)
+                {
+                    string savePath = Path.GetDirectoryName(sf.FileName);
+                    //另存文件
+                    Object oSavePath = savePath+"\\"+ dummyFileName;    //存檔路徑
+                    Object oFormat = Microsoft.Office.Interop.Word.WdSaveFormat.wdFormatDocument;    //格式
+                    myDoc.SaveAs(ref oSavePath, ref oFormat,
+                                ref oMissing, ref oMissing, ref oMissing, ref oMissing,
+                                ref oMissing, ref oMissing, ref oMissing, ref oMissing,
+                                ref oMissing, ref oMissing, ref oMissing, ref oMissing,
+                                ref oMissing, ref oMissing
+                            );
+                    //關閉檔案
+                    Object oFalse = false;
+                    myDoc.Close(ref oFalse, ref oMissing, ref oMissing);
+                    Console.WriteLine("創建完畢");
+                }
+
+                
+
+            
+
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //年份初始值
+            for (int i = DateTime.Now.Year - 5; i <= DateTime.Now.Year + 5; i++)
+            {
+                cbYear.Items.Add(i);
+            }
+            cbYear.Text = DateTime.Now.Year.ToString();
+            //月份初始值
+            for (int i = 1; i <= 12; i++)
+            {
+                cbMonth.Items.Add(i);
+            }
+            cbMonth.Text = DateTime.Now.Month.ToString();
+
+
+
+        }
+    }
+}
